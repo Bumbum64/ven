b51255534d0f844350427338b7ebadd0faf48165
BUMBUM64 <lolabr35@gmail.com>
2026-09-04 13:08:45 -0700

    Explicitly serve admin page

--- server.js
+++ server.js
@@ -53,6 +53,9 @@ app.use(
 );
 
 app.use(express.static(path.join(__dirname, "public")));
+app.get("/admin.html", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "admin.html"));
+});
 
 
 /* =========================
